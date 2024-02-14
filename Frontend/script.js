a32dc676faf3563be9d3b811ed13dcd3a6fc4d31
Dablu Kumar <krishnaroysilaur@gmail.com>
2024-02-14 19:26:08 +0530

    Admission form

---
+++ Frontend/script.js
@@ -0,0 +1,17 @@
+const Student = document.getElementById("link")
+const Container2 = document.getElementById("container2")
+Student.addEventListener('click',()=>{
+
+    Container2.style.display = "flex"
+    Container2.style.justifyContent = "space-between"
+    Container2.style.alignItems = "center"
+    Container2.style.flexDirection = "row"
+    Container2.style.height = "100vh"
+    Container2.style.width = "98%"
+    Container2.style.marginTop = "10%"
+})
+
+
+
+
+    
